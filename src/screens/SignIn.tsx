16b17b4dfd673af51b0c4fe0126acc6ff72d7f6a
Renan <renan.aragorn@gmail.com>
2022-07-21 00:24:23 -0300

    first commit, project started

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { Heading, VStack } from "native-base"
+
+export function SignIn(){
+  return(
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Heading>
+        Acesse sua conta
+      </Heading>
+    </VStack>
+  )
+}
