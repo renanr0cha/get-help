2967103c7dca7c4d3ce156827c102e5b916e19d4
Renan <renan.aragorn@gmail.com>
2022-07-24 12:34:25 -0300

    INput component done, working on signin screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,25 @@
-import { Heading, VStack } from "native-base"
+import { Heading, VStack, Icon, useTheme } from "native-base"
+import { Envelope, } from "phosphor-react-native"
+
+import Logo from "../assets/logo_primary.svg"
+import { Input } from "../components/Input"
 
 export function SignIn(){
+
+  const { colors } = useTheme()
   return(
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-      <Heading>
+      <Logo />
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6} >
         Acesse sua conta
       </Heading>
+      <Input 
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={ <Icon as={<Envelope color={colors.gray[300]}/>} />}
+      />
+      <Input placeholder="Senha" />
+
     </VStack>
   )
 }
