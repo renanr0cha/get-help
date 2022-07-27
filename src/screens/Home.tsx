fd391769ba61c7fb3a4138fa95b639bc93fa0410
Renan Rocha <renan.aragorn@gmail.com>
2022-07-27 16:21:27 -0300

    auth sign and sign out + firebase integration

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import auth from "@react-native-firebase/auth"
+import { Alert } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { HStack, IconButton, Text, useTheme, VStack, Heading, FlatList, Center } from 'native-base';
 import { SignOut, ChatTeardropText } from 'phosphor-react-native';
@@ -57,6 +59,15 @@ export function Home() {
     navigation.navigate("details", { orderId })
   }
 
+  function handleLogout(){
+    auth()
+      .signOut()
+      .catch(error => {
+        console.log(error)
+        return Alert.alert("Sair", "Não foi possível sair")
+      })
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -72,6 +83,7 @@ export function Home() {
 
         <IconButton
           icon={<SignOut size={26} color={colors.gray[300]}/>}
+          onPress={handleLogout}
         />
 
       </HStack>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import auth from "@react-native-firebase/auth"
 import { Heading, VStack, Icon, useTheme } from "native-base"
 
 import { Envelope, Key } from "phosphor-react-native"
@@ -6,25 +7,56 @@ import { Envelope, Key } from "phosphor-react-native"
 import Logo from "../assets/logo_primary.svg"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
+import { Alert } from "react-native"
 
 
 export function SignIn(){
-  const [name, setName] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
 
   const { colors } = useTheme()
+
+  function handleSignIn(){
+    if(!email || !password) {
+      return Alert.alert("Erro ao entrar", "Informe seu e-mail e senha")
+    }
+
+    setIsLoading(true)
+
+    auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => {
+      setIsLoading(false)
+      
+      if(error.code === "auth/invalid-email") {
+        return Alert.alert("Erro ao entrar", "E-mail inválido.")
+      }
+
+      if(error.code === "auth/wrong-password") {
+        return Alert.alert("Erro ao entrar", "E-mail ou senha inválida.")
+      }
+
+      if(error.code === "auth/user-not-found") {
+        return Alert.alert("Erro ao entrar", "E-mail ou senha inválida.")
+      }
+
+      return Alert.alert("Erro ao entrar", "Erro desconhecido")
+    })
+
+  }
   return(
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6} >
-        Acesse sua conta {name}
+        Acesse sua conta
       </Heading>
       <Input 
         placeholder="E-mail"
         mb={4}
         InputLeftElement={ <Icon as={<Envelope color={colors.gray[300]}/>} ml={4}/>}
-        onChangeText={setName}
+        onChangeText={setEmail}
       />
       <Input 
         placeholder="Senha"
@@ -34,7 +66,12 @@ export function SignIn(){
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full"/>
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
 
     </VStack>
   )
